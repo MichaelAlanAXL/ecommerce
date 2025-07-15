eb6bf7c9d8725342b3d5f5eb7e45aad3a4a059a3
MichaelAlanAXL <michael.dtna@gmail.com>
2025-07-14 23:34:29 -0300

    Improve account layout and fix active menu highlight

--- res/site/js/main.js
+++ res/site/js/main.js
@@ -1,98 +1,29 @@
-// jQuery(document).ready(function($){
-    
-//     // jQuery sticky Menu
-    
-// 	$(".mainmenu-area").sticky({topSpacing:0});
-    
-    
-//     $('.product-carousel').owlCarousel({
-//         loop:true,
-//         nav:true,
-//         margin:20,
-//         responsiveClass:true,
-//         responsive:{
-//             0:{
-//                 items:1,
-//             },
-//             600:{
-//                 items:3,
-//             },
-//             1000:{
-//                 items:5,
-//             }
-//         }
-//     });  
-    
-//     $('.related-products-carousel').owlCarousel({
-//         loop:true,
-//         nav:true,
-//         margin:20,
-//         responsiveClass:true,
-//         responsive:{
-//             0:{
-//                 items:1,
-//             },
-//             600:{
-//                 items:2,
-//             },
-//             1000:{
-//                 items:2,
-//             },
-//             1200:{
-//                 items:3,
-//             }
-//         }
-//     });  
-    
-//     $('.brand-list').owlCarousel({
-//         loop:true,
-//         nav:true,
-//         margin:20,
-//         responsiveClass:true,
-//         responsive:{
-//             0:{
-//                 items:1,
-//             },
-//             600:{
-//                 items:3,
-//             },
-//             1000:{
-//                 items:4,
-//             }
-//         }
-//     });    
-    
-    
-//     // Bootstrap Mobile Menu fix
-//     $(".navbar-nav li a").click(function(){
-//         $(".navbar-collapse").removeClass('in');
-//     });    
-    
-//     // jQuery Scroll effect
-//     $('.navbar-nav li a, .scroll-to-up').bind('click', function(event) {
-//         var $anchor = $(this);
-//         var headerH = $('.header-area').outerHeight();
-//         $('html, body').stop().animate({
-//             scrollTop : $($anchor.attr('href')).offset().top - headerH + "px"
-//         }, 1200, 'easeInOutExpo');
 
-//         event.preventDefault();
-//     });    
-    
-//     // Bootstrap ScrollPSY
-//     $('body').scrollspy({ 
-//         target: '.navbar-collapse',
-//         offset: 95
-//     })      
-// });
 
-//   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-//   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+document.addEventListener('DOMContentLoaded', () => {
+  const links = document.querySelectorAll('#menu-profile a');
+  const urlAtual = window.location.pathname;
 
-//   ga('create', 'UA-10146041-21', 'auto');
-//   ga('send', 'pageview');
+    // console.log('URL atual:', urlAtual);
+
+  links.forEach(link => {
+    const href = link.getAttribute('href');
+    // console.log('Verificando link:', href);
+
+    if (href === urlAtual) {
+      // console.log('Match! Adicionando classe active');
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+
+    // if (link.getAttribute('href') === urlAtual) {
+    //   link.classList.add('active');
+    // } else {
+    //   link.classList.remove('active');
+    // }
+  });
+});
 
   const swiper = new Swiper('.swiper', {
     loop: true,
